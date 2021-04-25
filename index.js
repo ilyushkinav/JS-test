f26231c322fd2c1652a40f10662f4097c0ec5505
Alexay <ilyushkinav@gmail.com>
2021-04-25 23:46:02 +0300

    start app

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 'use strict';
 
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', 0);
 
+let personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
 
+const addMovies = () => {
+  let lastFilm = '',
+      rating = '';
+  for (let i = 0; i < 2; i++) {
+    lastFilm = prompt('Один из последних просмотренных фильмов?');
+    rating = prompt('На сколько оцените его?');
 
-function Accumulator(startingValue) {
-  this.value = startingValue;
-  this.read = function() {
-    this.value += +prompt('Inter number', 0);
+    personalMovieDB.movies[lastFilm] = rating;
   };
-}
+};
 
-let acc = new Accumulator(1);
-
+addMovies();
