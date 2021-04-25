1977c4582ebcbd642344e379df8257e99cb9884d
Alexay <ilyushkinav@gmail.com>
2021-04-25 22:18:41 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+
+function Accumulator(startingValue) {
+  this.value = startingValue;
+  this.read = function() {
+    this.value += +prompt('Inter number', 0);
+  };
+}
+
+let acc = new Accumulator(1);
+
