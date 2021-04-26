2e9cd8a675696b86ffce7813c2ba176aa2b3efb8
Alexay <ilyushkinav@gmail.com>
2021-04-26 22:22:44 +0300

    update file

--- index.js
+++ index.js
@@ -10,15 +10,27 @@ let personalMovieDB = {
   privat: false,
 };
 
-const addMovies = () => {
+
+for (let i = 0; i < numberOfFilms; i++) {
   let lastFilm = '',
       rating = '';
-  for (let i = 0; i < 2; i++) {
-    lastFilm = prompt('Один из последних просмотренных фильмов?');
-    rating = prompt('На сколько оцените его?');
 
+  lastFilm = prompt('Один из последних просмотренных фильмов?', '');
+  rating = prompt('На сколько оцените его?', '');
+
+  if (lastFilm === '' || rating === '' || lastFilm === null || rating === null || lastFilm.length > 50) {
+    i--;
+  } else {
     personalMovieDB.movies[lastFilm] = rating;
-  };
+  }
 };
 
-addMovies();
+if (personalMovieDB.count > 0 && personalMovieDB.count < 10) {
+  alert('Просмотрено довольно мало фильмов.');
+} else if (personalMovieDB.count > 9 && personalMovieDB.count < 30) {
+  alert('Вы классический зритель.');
+} else if (29 < personalMovieDB.count) {
+  alert('Вы киноман.');
+} else {
+  alert('Произошла ошибка.');
+}
